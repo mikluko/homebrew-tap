83349dd13ddb1eca9cb33d233cb8aafc967acecc
Mikhail Lukianchenko <42915+mikluko@users.noreply.github.com>
2026-08-15 23:04:23 +0200

    block-goose-cli 1.46.0-mikluko.3: label wears the destination style

--- Formula/block-goose-cli.rb
+++ Formula/block-goose-cli.rb
@@ -1,9 +1,9 @@
 class BlockGooseCli < Formula
   desc "Open source, extensible AI agent (OSC 8 hyperlinks fork)"
   homepage "https://goose-docs.ai/"
-  url "https://github.com/mikluko/goose/archive/2251cc5e853e584ea3742e041507acd564549617.tar.gz"
-  version "1.46.0-mikluko.2"
-  sha256 "dedc7f96c147ae9e45bd9113a62eca22d8fd0a01c9c98e745e67153a926bef3c"
+  url "https://github.com/mikluko/goose/archive/37f844688777084e3c3e5c2f3ba7b2889afcecee.tar.gz"
+  version "1.46.0-mikluko.3"
+  sha256 "ead0891901315c2bae4498b367e93b87f9cc1e4652a3c6bc5bdb40f590f717d1"
   license "Apache-2.0"
   head "https://github.com/mikluko/goose.git", branch: "mikluko/osc8-hyperlinks"
 
